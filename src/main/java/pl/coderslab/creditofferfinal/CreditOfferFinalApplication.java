03665309157a2754c16dcd71c6f2c8099368e077
Brygida97 <krzywdziakbrygida97@gmail.com>
2023-05-15 22:49:24 +0200

    Add initial project structure

---
+++ src/main/java/pl/coderslab/creditofferfinal/CreditOfferFinalApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.creditofferfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditOfferFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditOfferFinalApplication.class, args);
+	}
+
+}
